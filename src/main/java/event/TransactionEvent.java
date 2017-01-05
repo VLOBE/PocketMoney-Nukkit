7f820efdfe17af8d120538630f0c2b27078c299e
06699 <aymen_z05@yahoo.fr>
2017-01-05 20:56:25 +0100

    Update TransactionEvent.java

--- src/main/java/event/TransactionEvent.java
+++ src/main/java/event/TransactionEvent.java
@@ -7,7 +7,35 @@ public class TransactionEvent extends PocketMoneyEvent{
 {
   
   const TRANSACTION_PAY = 0;
-
+  
+  private String sender;
+  private String receiver;
+  private int amount;
+  
+  public TransactionEvent(PocketMoney plugin,Strign _sender,String _receiver,int _amount) {
+    this.sender = _sender;
+    this.receiver = _receiver;
+    this.amount = _amount;
+    super(plugin);
+  }
+  
+  // Get sender name
+  // Return string 
+  public String getSender() {
+   return sender; 
+  }
+  
+  // Get receiver name
+  // Return string 
+  public String getReceiver() {
+    return receiver;
+  }
+  
+  // Get amount 
+  // Return integer 
+  public int getAmount() {
+    return amount; 
+  }
     //public static $handlerList = null;
     ///** @var string $sender */
     //private $sender;
