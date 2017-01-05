6baef857f0b4d1e8275c907c474c14b7bccaaa07
06699 <aymen_z05@yahoo.fr>
2017-01-05 21:07:45 +0100

    Update MoneyUpdateEvent.java

--- src/main/java/event/MoneyUpdateEvent.java
+++ src/main/java/event/MoneyUpdateEvent.java
@@ -2,8 +2,42 @@ package MoneyUpdateEvent;
 
 import cn.nukkit.Player;
 import Pocketmoney.PocketMoney;
+import cn.nukkit.event.HandlerList;
 
 public class MoneyUpdateEvent extends PocketMoneyEvent { 
+    
+    public static HandlerList handlerList = null;
+    
+    public static final int CAUSE_PAY = 0;
+    public static final int CAUSE_GRANT = 1;
+    public static final int CAUSE_SET = 2;
+    
+    private Player player;
+    private int amount;
+    private int cause;
+    
+    public MoneyUpdateEvent(PocketMoney plugin,Player _player,int _amount,int _cause) {
++        this.player = _player;
++        this.amount = _amount;
++        this.cause = _cause;
++        super(plugin);
++  }
+    
+    public Player getPlayer() {
+        return this.player;   
+    }
+    
+    public String getPlayerName() {
+        return this.player.getName();   
+    }
+    
+    public int getAmount() {
+        return this.amount;   
+    }
+    
+    public int getCause() {
+        return this.cause;   
+    }
     
     /*
     const CAUSE_PAY = 0;
